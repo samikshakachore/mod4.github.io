d85b758fffa3919ffbb90d3f5f4996d9c30aa194
samikshakachore <64363271+samikshakachore@users.noreply.github.com>
2020-07-27 08:50:52 +0530

    Update mod4.js

--- mod4.js
+++ mod4.js
@@ -1,16 +1,11 @@
-//Ensures there will be no 'console is undefined' errors
-window.console = window.console || (function(){
-    var c = {}; c.log = c.warn = c.debug = c.info = c.error = c.time = c.dir = c.profile = c.clear = c.exception = c.trace = c.assert = function(s){};
-    return c;
-})();
-
 //Actual code
-var names = ["Lydia","Alexa","Josephine","Emery","Julia","Delilah","Arianna","Vivian","Kaylee","Sophie","Brielle","Madeline","Peyton"];
-for (var i=0;i<names.length;i++){
+let names = ["Yaakov","John","Jane","Jeson","paul","Frank","Larry","Paula","Laura","Jim"];
+console.log(names);
+for ( let  i = 0; i<names.length; i++){
 	if (names[i].startsWith("J")){
-		console.log("Goodbye"+ names[i])
+		console.log("Goodbye " + names[i]);
     }
 	else{
-		console.log("Hello"+ names[i])
+		console.log("Hello "+ names[i]);
 	}
 }
